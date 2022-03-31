2820299a51e97d1433dff3746dbb58dd126943fc
dpolo-jamar <84636480+dpolo-jamar@users.noreply.github.com>
2022-03-31 12:26:49 -0500

    variables en prd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  domain: 'https://prueba-indra-back.herokuapp.com',
+  apis: {
+    consultar: '/api/consultar',
+    almacenar: '/api/almacenar',
+    eliminar: '/api/eliminar',
+  }
 };
